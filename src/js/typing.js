aa1b965295c28e2795d5f7665229aba7d5ef96a1
tbold <tanabold2@gmail.com>
2023-05-21 12:22:29 -0700

    add job description

--- src/js/typing.js
+++ src/js/typing.js
@@ -2,7 +2,7 @@ new TypeIt("#typewriter", {
   speed: 50,
   loop: true
 }).pause(750)
-  .type("Computer Science Student", {delay: 2000})
+  .type("World traveller", {delay: 2000})
   .delete(null, {delay: 1000})
-  .type("Junior Developer", {delay: 2000})
+  .type("Full-stack developer", {delay: 2000})
   .go();
